99709c3f47534f927e3f9175f5e666a2c61606c7
qwfsdaf <xogh990@gmail.com>
2024-03-13 14:00:48 +0900

    first commit

--- Project3/소스.c
+++ Project3/소스.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void) {
-	printf("asdfasdfasdf");
+int main(void) {	
+	int result = 0;
+	int k = 0;
+	for (int i = 2; i <= 100; i++) {
+		for (k = 2; k < i; k++) {
+			if (i % k == 0) break;
+		}
+		if (k == i) result += k;
+	}
+	printf("0부터 100까지의 소수의 합 : %d", result);
 }
